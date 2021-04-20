224bd553e48774a3ff5b8f6e6b208332375be8fe
JoannaCoPo <jococopo@gmail.com>
2021-04-20 15:40:26 -0600

    Final commit

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,14 +1,41 @@
 class FoodTruck
   attr_reader :name,
-              :inventory
+              :inventory,
+              :current_stock
 
   def initialize(name)
     @name = name
     @inventory = {}
-    @stock = 0
+    @current_stock = 0
   end
 
-  def check_stock(item)
-    @stock
+  def check_stock(item) #i dislike this strongly
+    accum = []
+    @inventory.each do |k, v|
+      if k == item
+        accum << v
+      end
+    end
+    @current_stock += accum.shift
   end
+
+  def stock(item, quantity)
+    @inventory[item] = quantity
+  end
+
+
+
+#had a plan here, can't remember what it was
+  def quantities
+    @inventory.values
+  end
+
+  def quantity_comparison #can't remember why i started this way
+    single_quantity = quantities.find do |quantity|
+      @inventory.has_value?(quantity)
+      quantity
+    end
+    single_quantity
+  end
+
 end
--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -19,8 +19,21 @@ RSpec.describe FoodTruck do
   it 'can check item stock' do
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
 
     expect(food_truck.check_stock(item1)).to eq(0)
+  end
+
+  it 'can stock items with quantity' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+    expect(food_truck.inventory).to eq({})
+
+    food_truck.stock(item1, 30)
+    # food_truck.stock(item2, 10)
+
+    # expect(food_truck.inventory).to eq({item1 => 30})
+    expect(food_truck.check_stock(item1)).to eq(30)
   end
 end
