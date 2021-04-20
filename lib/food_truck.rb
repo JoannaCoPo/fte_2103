18eb75c540f4d2480002db25830c8a2cb31ddc51
JoannaCoPo <jococopo@gmail.com>
2021-04-20 13:27:58 -0600

    Ensure FoodTruck and Item are interacting correctly

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,2 +1,14 @@
 class FoodTruck
+  attr_reader :name,
+              :inventory
+
+  def initialize(name)
+    @name = name
+    @inventory = {}
+    @stock = 0
+  end
+
+  def check_stock(item)
+    @stock
+  end
 end
